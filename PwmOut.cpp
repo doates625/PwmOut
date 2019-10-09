652d84076bfda74cc1475c84da1009dd1e5693fa
Dan <doates625@gmail.com>
2019-10-09 17:38:24 -0400

    Tested on Arduino (Mbed board got bricked, but API is identical and validated by Mbed)

---
+++ PwmOut.cpp
@@ -0,0 +1,59 @@
+/**
+ * @file PwmOut.h
+ * @author Dan Oates (WPI Class of 2020)
+ */
+#include "PwmOut.h"
+#include <CppUtil.h>
+
+#if defined(PLATFORM_ARDUINO)
+
+/**
+ * @brief Constructs PWM output and writes 0% duty cycle
+ * @param pin Platform-specific pin ID
+ */
+PwmOut::PwmOut(PLATFORM_PIN_TYPE pin)
+{
+	this->pin = pin;
+	write(0.0f);
+}
+
+/**
+ * @brief Sets PWM to given duty cycle
+ * @param duty_cycle Duty cycle in range [0,1]
+ * 
+ * An input of 0 corresponds to 0% and 1 to 100%. Inputs outside of this range
+ * are automatically clamp limited.
+ */
+void PwmOut::write(float duty_cycle)
+{
+	this->duty_cycle = clamp_limit(duty_cycle, 0.0f, 1.0f);
+	analogWrite(pin, this->duty_cycle * 255.0f);
+}
+
+/**
+ * @brief Returns current duty cycle
+ */
+float PwmOut::read()
+{
+	return duty_cycle;
+}
+
+/**
+ * @brief Shorthand for write operation
+ * @param duty_cycle Duty cycle in range [0,1]
+ */
+PwmOut& PwmOut::operator=(float duty_cycle)
+{
+	write(duty_cycle);
+	return *this;
+}
+
+/**
+ * @brief Shorthand for read operation
+ */
+PwmOut::operator float()
+{
+	return read();
+}
+
+#endif
---
+++ PwmOut.h
@@ -0,0 +1,39 @@
+/**
+ * @brief PwmOut.h
+ * @brief Class for Arduino and Mbed PWM outputs
+ * @author Dan Oates (WPI Class of 2020)
+ * 
+ * This class implements the Mbed PwmOut class for Arduino to allow for
+ * cross-platform libraries. This implementation does NOT include the period and
+ * pulsewidth methods offered by Mbed as this level of control is not allowed by
+ * the standard Arduino library.
+ * 
+ * Dependencies:
+ * - Platform: https://github.com/doates625/Platform.git
+ * - CppUtil: https://github.com/doates625/CppUtil.git
+ * 
+ * References:
+ * - Mbed PwmOut: https://os.mbed.com/docs/mbed-os/v5.14/apis/pwmout.html
+ */
+#pragma once
+#include <Platform.h>
+
+#if defined(PLATFORM_ARDUINO)
+
+/**
+ * Class Declaration
+ */
+class PwmOut
+{
+public:
+	PwmOut(PLATFORM_PIN_TYPE pin);
+	void write(float duty_cycle);
+	float read();
+	PwmOut& operator=(float duty_cycle);
+	operator float();
+private:
+	PLATFORM_PIN_TYPE pin;
+	float duty_cycle;
+};
+
+#endif
